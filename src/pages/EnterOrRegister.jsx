e00178501983a8ba8e141f50569da7a08990bdd8
Roman Nichi <roman.nichi.o@gmail.com>
2023-04-14 13:19:27 +0300

    change margins

--- src/pages/EnterOrRegister.jsx
+++ src/pages/EnterOrRegister.jsx
@@ -4,16 +4,16 @@ export const EnterOrRegister = () =>{
     const enterForm = 
         <form className="d-flex flex-column px-5 py-1">
             <h1 className="d-flex justify-content-center">Войти в профиль</h1>
-            <input className="form-control m-1" type="text" placeholder="Логин"/>
-            <input className="form-control m-1" type="text" placeholder="Пароль"/>
-            <div className="d-flex flex-row justify-content-around">
+            <input className="form-control form-control-lg my-2" type="text" placeholder="Логин"/>
+            <input className="form-control form-control-lg my-2" type="text" placeholder="Пароль"/>
+            <div className="d-flex flex-row justify-content-between">
                 <div className="flex-row">
                     <input className="m-1" type="checkbox" name="remember-me" id="" />
                     <label className="m-1" htmlFor="remember-me">Запомнить меня</label>
                 </div>
                 <button className="btn btn-outline-secondary border-0 text-button">Забыли пароль?</button>
             </div>
-            <button className="btn btn-primary px-5 py-1">Войти</button>
+            <button className="btn btn-primary px-3 py-3"><h4>Войти</h4></button>
             <button className="btn btn-outline-secondary border-0 text-button">Зарегистрироваться</button>
             <hr />
             <p className="d-flex justify-content-center">Другие способы входа</p>
@@ -25,16 +25,16 @@ export const EnterOrRegister = () =>{
     const registerForm = 
         <form className="d-flex flex-column px-5 py-1">
             <h1 className="d-flex justify-content-center">Регистрация</h1>
-            <input className="form-control m-1" type="text" placeholder="Логин"/>
-            <input className="form-control m-1" type="text" placeholder="Пароль"/>
-            <input className="form-control m-1" type="text" placeholder="Повтор пароля"/>
-            <button className="m-1 btn btn-primary">Зарегестрироваться</button>
-            <button>Войти в профиль</button>
+            <input className="form-control form-control-lg my-2" type="text" placeholder="Логин"/>
+            <input className="form-control form-control-lg my-2" type="text" placeholder="Пароль"/>
+            <input className="form-control form-control-lg my-2" type="text" placeholder="Повтор пароля"/>
+            <button className="btn btn-primary px-3 py-3"><h4>Зарегестрироваться</h4></button>
+            <button className="btn btn-outline-secondary border-0 text-button">Войти в профиль</button>
         </form>
 
     return(
         <div className="d-flex justify-content-center">
-            {enterForm}
+            {registerForm}
         </div>
     )
 }
