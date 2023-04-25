eee2f3a2fd974e662af949b357fa5c43b2f14a4f
Marika777 <marika.meidra2003@gmail.com>
2023-04-25 22:47:01 +0400

    btn-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,31 @@ export const Header = () =>{
             <a href=""><img src={require("../assets/icons/header-logo.png")} alt="Логотип сайта" className="logo" /></a>
             <div className="d-flex flex-row">
                 <Search />
-                <a href=""><img src={require("../assets/icons/header-entry.png")} alt="Вход" className="log-in" /></a>
-                <a href=""><img src={require("../assets/icons/header-bookshelf.png")} alt="Книжная полка" className="book-shelf" /></a>
-                <a href=""><img src={require("../assets/icons/header-gift.png")} alt="Подарок" className="gift" /></a>
+                <div className="d-flex flex-pow btn-header">
+                    <div className=" d-flex flex-column center-elements">
+                        <a href="">
+                            <img src={require("../assets/icons/header-entry.png")} alt="Вход" className="log-in" />
+                            <p>Вход</p>
+                        </a>
+                    </div>
+                    <div className="d-flex flex-column center-elements">
+                        <a href="">
+                            <img src={require("../assets/icons/header-bookshelf.png")} alt="Книжная полка" className="book-shelf" />
+                            <p>Книжная полка</p>
+                        </a>
+                        </div>
+                    <div className="d-flex flex-column center-elements">
+                        <a href="">
+                            <img src={require("../assets/icons/header-gift.png")} alt="Подарок" className="gift" />
+                            <p>Подарок</p>
+                        </a>
+                        </div>
+                </div>
             </div>
             <div className="d-flex flex-row">
                 <nav>
                     <ul className="d-flex flex-row" style={undottedUL}>
-                        <a><img src="" alt="" className="log-in" /><li>Новинки</li></a>
+                        { <a>/*<img src="" alt="" className="log-in" />*/<li>Новинки</li></a>}
                         <a><li>Аудиокниги</li></a>
                         <a><li>Подборки</li></a>
                         <a><li>Подписки</li></a>
