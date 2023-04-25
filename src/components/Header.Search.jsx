1329782035c54ffb80a91936c0eb56ba0868a7e0
Marika777 <marika.meidra2003@gmail.com>
2023-04-25 21:01:46 +0400

    search

--- src/components/Header.Search.jsx
+++ src/components/Header.Search.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "../css/Header.Search.css"
 
 export default function Search(){
     return(
-        <input type="text" />
+        <div className="d-flex flex-row search">
+            <button className="btn btn-primary my-1 px-3 py-3 advanced-search position-relative">
+                <img src={require("../assets/icons/header-advanced-search.png")} alt="Расширинный поиск" className="img-advanced-search position-absolute top-50 start-50 translate-middle" />
+            </button>
+            <input type="text" />
+            <button className="btn btn-primary my-1 px-3 py-3 search-btn position-relative">
+                <img src={require("../assets/icons/header-search.png")} alt="Поиск" className="img-search position-absolute top-50 start-50 translate-middle" />
+            </button>
+        </div>
     )
 }
+
+// style = {{
+//     'minWidth': 100 + '%',
+//     'maxWidth': 100 + '%',
+// }}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./Header.Search";
+import "../css/Header.css"
 
 export const Header = () =>{
     const undottedUL = {
@@ -7,18 +8,18 @@ export const Header = () =>{
     }
 
     return(
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-row header">
+            <a href=""><img src={require("../assets/icons/header-logo.png")} alt="Логотип сайта" className="logo" /></a>
             <div className="d-flex flex-row">
-                <a href=""><img src="" alt="" className="logo" /></a>
                 <Search />
-                <a href=""><img src="" alt="" className="log-in" /></a>
-                <a href=""><img src="" alt="" className="book-shelf" /></a>
-                <a href=""><img src="" alt="" className="gift" /></a>
+                <a href=""><img src={require("../assets/icons/header-entry.png")} alt="Вход" className="log-in" /></a>
+                <a href=""><img src={require("../assets/icons/header-bookshelf.png")} alt="Книжная полка" className="book-shelf" /></a>
+                <a href=""><img src={require("../assets/icons/header-gift.png")} alt="Подарок" className="gift" /></a>
             </div>
             <div className="d-flex flex-row">
                 <nav>
                     <ul className="d-flex flex-row" style={undottedUL}>
-                        <a><li>Новинки</li></a>
+                        <a><img src="" alt="" className="log-in" /><li>Новинки</li></a>
                         <a><li>Аудиокниги</li></a>
                         <a><li>Подборки</li></a>
                         <a><li>Подписки</li></a>
