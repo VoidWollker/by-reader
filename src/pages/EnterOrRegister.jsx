baf01ee8a3bb14c8312b7109538a4bd5dcde4bfe
Marika777 <marika.meidra2003@gmail.com>
2023-04-27 00:57:46 +0400

    регистрация

--- src/pages/EnterOrRegister.jsx
+++ src/pages/EnterOrRegister.jsx
@@ -24,17 +24,17 @@ export const EnterOrRegister = () =>{
 
     const registerForm = 
         <form className="d-flex flex-column px-5 py-1">
-            <h1 className="d-flex justify-content-center">Регистрация</h1>
-            <input className="form-control form-control-lg my-2" type="text" placeholder="Логин"/>
-            <input className="form-control form-control-lg my-2" type="text" placeholder="Пароль"/>
-            <input className="form-control form-control-lg my-2" type="text" placeholder="Повтор пароля"/>
-            <button className="btn btn-primary my-1 px-3 py-3"><h4>Зарегестрироваться</h4></button>
+            <h1 className="d-flex justify-content-center mt-3">Регистрация</h1>
+            <input className="form-control form-control-lg my-2 ms-0" type="text" placeholder="Логин"/>
+            <input className="form-control form-control-lg my-2 ms-0" type="text" placeholder="Пароль"/>
+            <input className="form-control form-control-lg my-2 ms-0" type="text" placeholder="Повтор пароля"/>
+            <button className="btn btn-primary my-1 px-3 py-3 btn-registration"><h4>Зарегестрироваться</h4></button>
             <button className="btn btn-outline-secondary border-0 text-button">Войти в профиль</button>
         </form>
 
     return(
         <div className="d-flex justify-content-center">
-            {registerForm}
+            {enterForm}
         </div>
     )
 }
