e271db22de823089781eea81a989d9effb04642f
Roman Nichi <roman.nichi.o@gmail.com>
2023-05-05 16:39:51 +0300

    product

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,28 +1,31 @@
+import "../css/ProductDetails.css"
+
 export const ProductDetials = ({title, format, author, recomendedAge, publisher, seria, rate, ratesCount, reviewCount}) =>{
     return(
         <div className="d-flex flex-row">
-            <div className="d-flex flex-column"></div>
             <div className="d-flex flex-column">
-                <div className="d-flex flex-row">
-                    <p className="product-title">{title}</p>
-                    <p className="product-format"></p>
-                    <p className="product-recomended-age">+{recomendedAge}</p>
+                <img src={require("../assets/books/Дизайн 10.png")} alt="" />
+            </div>
+            <div className="d-flex flex-column">
+                <div className="d-flex flex-row mb-3">
+                    <p className="product-title"><b>{title}</b></p>
+                    <div className="produсе-feature ">
+                        <p className="product-format">{format}</p>
+                    </div>
+                    <div className="produсе-feature mx-2">
+                        <p className="product-recomended-age">{recomendedAge}+</p>
+                    </div>
                 </div>
-                <div className="d-flex flex-row">
-                    <p>Автор:</p>
-                    <p>{author}</p>
+                <div className="d-flex flex-column data-book">
+                    <p>Автор: {author}</p>
+                    <p>Издательство: {publisher}</p>
+                    {seria !== null ?
+                        <div className="d-flex flex-row">
+                            <p>Серия: {seria}</p>
+                        </div> :
+                        ''
+                    }
                 </div>
-                <div className="d-flex flex-row">
-                    <p>Издательство:</p>
-                    <p>{publisher}</p>
-                </div>
-                {seria !== null ?
-                    <div className="d-flex flex-row">
-                        <p>Серия:</p>
-                        <p>{seria}</p>
-                    </div> :
-                    ''
-                }
                 <div className="d-flex flex-row">
                     <div className="d-flex-flex-column">
                         <div className="d-flex flex-row">
@@ -36,6 +39,7 @@ export const ProductDetials = ({title, format, author, recomendedAge, publisher,
                             <img src="" alt="" />
                             <p>{reviewCount}</p>
                         </div>
+                        <p>Отзывы</p>
                     </div>
                 </div>
             </div>
