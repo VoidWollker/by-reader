a9d91dc9996d00f4d55a943d304f31b5c948052a
Marika777 <marika.meidra2003@gmail.com>
2023-05-12 23:35:43 +0400

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,21 +8,21 @@ export const Header = () =>{
     }
 
     return(
-        <header className="d-flex flex-row">
-            <a href=""><img src={require("../assets/icons/header-logo.png")} alt="Логотип сайта" className="logo" /></a>
-            <div className="d-flex flex-row">
+        <header className="d-flex flex-row ">
+            <a href=""><img src={require("../assets/icons/header-logo.png")} alt="Логотип сайта" className="logo mb-3" /></a>
+            <div className="d-flex flex-row flex-wrap justify-content-center">
                 <div className="d-flex flex-column search-and-menu">    
                     <Search />
                     <div className="d-flex flex-row position-relative">
                         <nav>
-                            <ul className="d-flex flex-row menu position-absolute top-0 start-0" style={undottedUL}>
-                                <a className="d-flex flex-row"><img src={require("../assets/icons/header-popular.png")} alt="" className="mini-icons" /><li>Популярное</li></a>
-                                <a className="d-flex flex-row"><img src={require("../assets/icons/header-new.png")} alt="" className="mini-icons" /><li>Новинки</li></a>
-                                <a className="d-flex flex-row w-min"><img src={require("../assets/icons/header-exclusive.png")} alt="" className="mini-icons" /><li>Эксклюзив от Почитателя</li></a>
-                                <a className="d-flex flex-row"><img src={require("../assets/icons/header-audiobook.png")} alt="" className="mini-icons" /><li>Аудиокниги</li></a>
-                                <a className="d-flex flex-row"><img src={require("../assets/icons/header-podcasts.png")} alt="" className="mini-icons" /><li>Подкасты</li></a>
-                                <a className="d-flex flex-row"><img src={require("../assets/icons/header-genres.png")} alt="" className="mini-icons" /><li>Жанры</li></a>
-                                <a className="d-flex flex-row"><img src={require("../assets/icons/header-subscription.png")} alt="" className="mini-icons" /><li>Подписки</li></a>
+                            <ul className="d-flex flex-row  menu position-absolute top-0 start-0 flex-container flex-wrap" style={undottedUL}>
+                                <a className="d-flex flex-row "><img src={require("../assets/icons/header-popular.png")} alt="" className="mini-icons" /><li>Популярное</li></a>
+                                <a className="d-flex flex-row "><img src={require("../assets/icons/header-new.png")} alt="" className="mini-icons" /><li>Новинки</li></a>
+                                <a className="d-flex flex-row w-min " ><img src={require("../assets/icons/header-exclusive.png")} alt="" className="mini-icons" /><li>Эксклюзив от Почитателя</li></a>
+                                <a className="d-flex flex-row "><img src={require("../assets/icons/header-audiobook.png")} alt="" className="mini-icons" /><li>Аудиокниги</li></a>
+                                <a className="d-flex flex-row "><img src={require("../assets/icons/header-podcasts.png")} alt="" className="mini-icons" /><li>Подкасты</li></a>
+                                <a className="d-flex flex-row "><img src={require("../assets/icons/header-genres.png")} alt="" className="mini-icons" /><li>Жанры</li></a>
+                                <a className="d-flex flex-row " ><img src={require("../assets/icons/header-subscription.png")} alt="" className="mini-icons" /><li>Подписки</li></a>
                             </ul>
                         </nav>
                     </div>
@@ -37,9 +37,9 @@ export const Header = () =>{
                     <div className="d-flex flex-column center-elements">
                         <a href="">
                             <img src={require("../assets/icons/header-bookshelf.png")} alt="Книжная полка" className="book-shelf" />
-                            <p>Книжная полка</p>
+                            <p className="bookshelf">Книжная полка</p>
                         </a>
-                        </div>
+                    </div>
                     <div className="d-flex flex-column center-elements">
                         <a href="">
                             <img src={require("../assets/icons/header-gift.png")} alt="Подарок" className="gift" />
