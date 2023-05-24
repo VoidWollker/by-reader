509139aba4119c4b0f369769485eea9c58bbcfa1
sophiakars <sofia.karsakova@gmail.com>
2023-05-24 13:38:49 +0400

    update record.js, users.js, bookModel.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -43,4 +43,69 @@ recordRoutes.get('/:id', async (req, res) => {
   res.status(200).json(book)
 })
 
+// This section will help you get a single record by title
+recordRoutes.get('/find/bytitle', async (req, res, next) => {
+  const title = req.body.title
+
+  const book = await Book.find({'title': title})
+
+  if (book == '') {
+    return res.status(404).json({error: 'No such book'})
+  }
+
+  res.status(200).json(book)
+})
+
+// This section will help you get a single record by author
+recordRoutes.get('/find/byauthor', async (req, res, next) => {
+  const author = req.body.author
+
+  const book = await Book.find({'author': author})
+
+  if (book == '') {
+    return res.status(404).json({error: 'No such book'})
+  }
+
+  res.status(200).json(book)
+})
+
+// This section will help you get a single record by genre
+recordRoutes.get('/find/byagenre', async (req, res, next) => {
+  const genre = req.body.genre
+
+  const book = await Book.find({'genre': genre})
+
+  if (book == '') {
+    return res.status(404).json({error: 'No such book'})
+  }
+
+  res.status(200).json(book)
+})
+
+// This section will help you get a single record by publisher
+recordRoutes.get('/find/bypublisher', async (req, res, next) => {
+  const publisher = req.body.publisher
+
+  const book = await Book.find({'publisher': publisher})
+
+  if (book == '') {
+    return res.status(404).json({error: 'No such book'})
+  }
+
+  res.status(200).json(book)
+})
+
+// This section will help you get a single record by seria
+recordRoutes.get('/find/byseria', async (req, res, next) => {
+  const seria = req.body.seria
+
+  const book = await Book.find({'seria': seria})
+
+  if (book == '') {
+    return res.status(404).json({error: 'No such book'})
+  }
+
+  res.status(200).json(book)
+})
+
 module.exports = recordRoutes;
