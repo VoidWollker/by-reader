872fcf354eef5dde9b07646cff201ee79aca8ebb
Roman Nichi <Roman.Nichi.O@gmail.com>
2023-06-21 15:12:24 +0400

    description work with paragraphs

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -3,9 +3,9 @@ import "../css/ProductDescription.css"
 export const ProductDescription = ({elementID, description}) =>{
     return(
         <>
-            <div className="w-75 mx-auto" id={elementID}>
+            <div className="d-flex flex-column w-75 mx-auto" id={elementID}>
                 <h1>Краткое описание книги</h1>
-                <p className="product-description">{description}</p>
+                <p className="product-description">{description.map(paragraph => <p>{paragraph}</p>)}</p>
             </div>
         </>
     )
