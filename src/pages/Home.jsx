aca98277d45fbdbafcf86304c09ec46ab0cbeae9
Roman Nichi <Roman.Nichi.O@gmail.com>
2023-06-21 15:01:19 +0400

    update ProducCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () =>{
 
     useEffect(() => {
         getBooks()
-    })
+    }, [])
 
     const getBooks = async () =>{
         await fetch('http://localhost:5000/book/all')
@@ -27,44 +27,27 @@ export const Home = () =>{
                 }}
             />
             <div className="d-flex flex-column w-75" style={{marginLeft: '12.5vw', marginRight: '12.5vw'}}>
-                <h2 className="my-4">Популярное</h2>
-                <ProductSlider 
-                    slidesPerView={5}
-                    slides={books.map(book =>{
-                        return <ProductCard 
-                            showType={'normal'}
-                            id={book._id}
-                            title={book.title}
-                            author={book.author}
-                            image={(book.cover).toString()}
-                            price={book.price}
-                            fakePrice={book.fakePrice}
-                            format={book.format}
-                            pageCount={book.pageCount}
-                            rate={book.rate}
-                            ratesCount={book.ratesCount}
-                        ></ProductCard>
-                    })}
-                />
                 <h2 className="my-4">Новинки</h2>
                 <ProductSlider 
                     slidesPerView={5}
-                    slides={[1,2,3,4,5,6,7,8].map(i =>{
+                    slides={books.sort((a,b) => a.createdAt - b.createdAt).map(book =>{
                         return <ProductCard 
-                            showType={'mini'}
-                            title={'1984'}
-                            author={'Джордж Оруэл'}
-                            image={'Дизайн 10.png'}
-                            price={810}
-                            fakePrice={920}
-                            format={'Текст'}
-                            pageCount={170}
-                            rate={4.8}
-                            countRates={640}
+                            showType={'normal'}
+                            book={book}
                         ></ProductCard>
                     })}
                 />
-                <h2 className="my-4">Аудиокниги</h2>
+                <h2 className="my-4">Фантастика</h2>
+                <ProductSlider 
+                    slidesPerView={5}
+                    slides={books.filter(book => book.genre === 'Фантастика').map(book =>{
+                        return <ProductCard 
+                        showType={'mini'}
+                        book={book}
+                        ></ProductCard>
+                    })}
+                />
+                {/* <h2 className="my-4">Аудиокниги</h2>
                 <ProductSlider 
                     slidesPerView={5}
                     slides={[1,2,3,4,5,6,7,8].map(i =>{
@@ -99,7 +82,7 @@ export const Home = () =>{
                             countRates={640}
                         ></ProductCard>
                     })}
-                />
+                /> */}
                 <h2 className="my-4">Подписки от Почитателя</h2>
                 <Subscriptions/>
             </div>
