0dd59841ea633b865621f52e9bba20ac48682ad8
Roman Nichi <Roman.Nichi.O@gmail.com>
2023-06-22 13:01:26 +0400

    add loading to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import "../css/Home.css"
 import { Loading } from "../components/Loading"
 
 export const Home = () =>{
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState(null)
 
     useEffect(() => {
         getBooks()
@@ -27,69 +27,71 @@ export const Home = () =>{
                     'maxHeight': 80 + 'vh'
                 }}
             />
-            <div className="d-flex flex-column w-75" style={{marginLeft: '12.5vw', marginRight: '12.5vw'}}>
-                <h2 className="my-4">Новинки</h2>
-                <ProductSlider 
-                    elementID={'newest'}
-                    slidesPerView={5}
-                    slides={books.sort((a,b) => a.createdAt - b.createdAt).map(book =>{
-                        return <ProductCard 
-                            showType={'normal'}
+            {books !== null ?
+                <div className="d-flex flex-column w-75" style={{marginLeft: '12.5vw', marginRight: '12.5vw'}}>
+                    <h2 className="my-4">Новинки</h2>
+                    <ProductSlider 
+                        elementID={'newest'}
+                        slidesPerView={5}
+                        slides={books.sort((a,b) => a.createdAt - b.createdAt).map(book =>{
+                            return <ProductCard 
+                                showType={'normal'}
+                                book={book}
+                            ></ProductCard>
+                        })}
+                    />
+                    <h2 className="my-4">Фантастика</h2>
+                    <ProductSlider 
+                        elementID={'fantstic'}
+                        slidesPerView={5}
+                        slides={books.filter(book => book.genre === 'Фантастика').map(book =>{
+                            return <ProductCard 
+                            showType={'mini'}
                             book={book}
-                        ></ProductCard>
-                    })}
-                />
-                <h2 className="my-4">Фантастика</h2>
-                <ProductSlider 
-                    elementID={'fantstic'}
-                    slidesPerView={5}
-                    slides={books.filter(book => book.genre === 'Фантастика').map(book =>{
-                        return <ProductCard 
-                        showType={'mini'}
-                        book={book}
-                        ></ProductCard>
-                    })}
-                />
-                <Loading/>
-                {/* <h2 className="my-4">Аудиокниги</h2>
-                <ProductSlider 
-                    slidesPerView={5}
-                    slides={[1,2,3,4,5,6,7,8].map(i =>{
-                        return <ProductCard 
-                            showType={'square'}
-                            title={'1984'}
-                            author={'Джордж Оруэл'}
-                            image={'Дизайн 10.png'}
-                            price={810}
-                            fakePrice={920}
-                            format={'Текст'}
-                            pageCount={170}
-                            rate={4.8}
-                            countRates={640}
-                        ></ProductCard>
-                    })}
-                />
-                <h2 className="my-4">Подкасты</h2>
-                <ProductSlider 
-                    slidesPerView={4}
-                    slides={[1,2,3,4,5,6,7,8].map(i =>{
-                        return <ProductCard 
-                            showType={'bigSquare'}
-                            title={'1984'}
-                            author={'Джордж Оруэл'}
-                            image={'Дизайн 10.png'}
-                            price={810}
-                            fakePrice={920}
-                            format={'Текст'}
-                            pageCount={170}
-                            rate={4.8}
-                            countRates={640}
-                        ></ProductCard>
-                    })}
-                /> */}
-                <h2 className="my-4">Подписки от Почитателя</h2>
-                <Subscriptions/>
-            </div>
+                            ></ProductCard>
+                        })}
+                    />
+                    {/* <h2 className="my-4">Аудиокниги</h2>
+                    <ProductSlider 
+                        slidesPerView={5}
+                        slides={[1,2,3,4,5,6,7,8].map(i =>{
+                            return <ProductCard 
+                                showType={'square'}
+                                title={'1984'}
+                                author={'Джордж Оруэл'}
+                                image={'Дизайн 10.png'}
+                                price={810}
+                                fakePrice={920}
+                                format={'Текст'}
+                                pageCount={170}
+                                rate={4.8}
+                                countRates={640}
+                            ></ProductCard>
+                        })}
+                    />
+                    <h2 className="my-4">Подкасты</h2>
+                    <ProductSlider 
+                        slidesPerView={4}
+                        slides={[1,2,3,4,5,6,7,8].map(i =>{
+                            return <ProductCard 
+                                showType={'bigSquare'}
+                                title={'1984'}
+                                author={'Джордж Оруэл'}
+                                image={'Дизайн 10.png'}
+                                price={810}
+                                fakePrice={920}
+                                format={'Текст'}
+                                pageCount={170}
+                                rate={4.8}
+                                countRates={640}
+                            ></ProductCard>
+                        })}
+                    /> */}
+                    <h2 className="my-4">Подписки от Почитателя</h2>
+                    <Subscriptions/>
+                </div> :
+                <Loading />
+            }
         </div>
     )
 }
