6b20d563ff180599a3eaba33c2f8e4bd4c550207
Roman Nichi <Roman.Nichi.O@gmail.com>
2023-06-22 23:15:28 +0400

    fiv reviews

--- src/components/ProductReviews.jsx
+++ src/components/ProductReviews.jsx
@@ -18,11 +18,7 @@ export const ProductReviews = ({elementID, currentUserName, reviews, reviewsCoun
                     </div>
                     <p className="p-1 ps-3">{text}</p>
                 </div>
-                    {/* <ClientEmpty
-                        icon={require("../assets/icons/reviews.png")}
-                        title={'Здесь будут отображаться отзывы'}
-                        description={'Напишите отзыв первым!'}            
-                    /> */}
+                    
             </div>
         )
     }
@@ -31,20 +27,31 @@ export const ProductReviews = ({elementID, currentUserName, reviews, reviewsCoun
     return(
         <div className="d-flex flex-column w-75 mx-auto" id={elementID}>
             <h1 className="pt-4">Отзывы</h1>
-            {reviews.map((review, index) =>
-                index <= reviewsCount - 1 ?
-                    createReview('', review.username, new Date(review.createdAt).toLocaleDateString('ru-RU'), 4, review.text) :
-                    ''
-            )}
-            {reviews.length > reviewsCount ?
-                <button className="btn btn-primary btn-reviews" onClick={increaseReviewsCount}>Ещё отзывы</button> :
-                ''
+            {reviews.length > 0 ?
+                <>
+                    {reviews.map((review, index) =>
+                        index <= reviewsCount - 1 ?
+                            createReview('', review.username, new Date(review.createdAt).toLocaleDateString('ru-RU'), 4, review.text) :
+                            ''
+                    )}
+                    {reviews.length > reviewsCount ?
+                        <button className="btn btn-primary btn-reviews" onClick={increaseReviewsCount}>Ещё отзывы</button> :
+                        ''
+                    }
+                </> :
+                <ClientEmpty
+                    icon={require("../assets/icons/reviews.png")}
+                    title={'Здесь будут отображаться отзывы'}
+                    description={'Напишите отзыв первым!'}            
+                />
+
             }
+            
             <h1 className="pt-4 pb-2">Оставьте отзыв</h1>
             <div className="d-flex flex-row review-add p-3 flex-wrap justify-content-center">
                 <p>{currentUserName}</p>
                 {/* <img src={require("../assets/icons/foto-avatar.png")} alt="" className="avatar mt-2"/> */}
-                <input className="add-review ps-2 mx-3 mb-2 mt-2" placeholder="Что вы думаете о книге?"
+                <input className="add-review ps-2 mx-3 mb-2 mt-2" placeholder="Что вы думаете о книге?" value={reviewText}
                     onChange={e => setReviewText(e.target.value)}    
                 />
                 <button className="btn btn-tertiary btn-addReview px-0 mt-1" onClick={() => writeReview(reviewText)}>Опубликовать</button>
